4aff3416d8e0b359209fae1f6e9d2e8470590f40
aarsyah0 <arsasaja2@gmail.com>
2023-12-07 13:18:31 +0700

    Create sidebar.php

---
+++ arsyah/webadmin/admin/sidebar.php
@@ -0,0 +1,60 @@
+<div class="navigation">
+            <ul>
+                <li class="headnav">
+                    <a href="#">
+                        <span class="icon">
+                            <img src="../assets/imgs/image 2.png">
+                        </span>
+                        <span class="title">JTI-VOTE</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="index.php?homepage=1">
+                        <span class="icon">
+                            <ion-icon name="home-outline"></ion-icon>
+                        </span>
+                        <span class="title">Dashboard</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="index.php?addCandidatePage=1">
+                        <span class="icon">
+                        <ion-icon name="people-outline"></ion-icon>
+                        </span>
+                        <span class="title">Add Candidates</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="index.php?addElectionPage=1">
+                        <span class="icon">
+                            <ion-icon name="chatbubble-outline"></ion-icon>
+                        </span>
+                        <span class="title">Add Election</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="index.php?addVoterPage=1">
+                        <span class="icon">
+                        <ion-icon name="person-outline"></ion-icon>
+                        </span>
+                        <span class="title">Add Voters</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="logout.php">
+                        <span class="icon">
+                            <ion-icon name="log-out-outline"></ion-icon>
+                        </span>
+                        <span class="title">Sign Out</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <!-- ========================= Main ==================== -->
+        <div class="main">
