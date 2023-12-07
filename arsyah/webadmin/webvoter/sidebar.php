d938f69c800fe4d405424d32c6ad893a99bc61a0
aarsyah0 <arsasaja2@gmail.com>
2023-12-07 13:25:09 +0700

    Create sidebar.php

---
+++ arsyah/webadmin/webvoter/sidebar.php
@@ -0,0 +1,33 @@
+<div class="navigation">
+            <ul>
+                <li class="headnav">
+                    <a href="#">
+                        <span class="icon">
+                            <img src="../assets/imgs/image 2.png">
+                        </span>
+                        <span class="title">JTI-VOTE</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="index.php?homepage=1">
+                        <span class="icon">
+                            <ion-icon name="home-outline"></ion-icon>
+                        </span>
+                        <span class="title">Dashboard</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="logout.php">
+                        <span class="icon">
+                            <ion-icon name="log-out-outline"></ion-icon>
+                        </span>
+                        <span class="title">Sign Out</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <!-- ========================= Main ==================== -->
+        <div class="main">
